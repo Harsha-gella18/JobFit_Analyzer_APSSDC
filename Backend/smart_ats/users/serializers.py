994a809d08e22f3d8258ead8cc3bb7e9117fcceb
harsha <hgella91@gmail.com>
2025-06-27 12:06:12 +0530

    First commit upto auth

---
+++ Backend/smart_ats/users/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from .models import User
+
+class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'confirm_password']
+
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+        return User.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(email=data['email'], password=data['password'])
+        if not user:
+            raise serializers.ValidationError("Invalid credentials.")
+        data['user'] = user
+        return data
